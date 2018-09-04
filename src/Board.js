e58a95480db33331f99a480214d8562fcce0b6b9
m0nq <forgetmenaught@gmail.com>
2018-09-03 19:19:50 -0700

    Have initial application state fetch from the bacon ipsum api

--- src/Board.js
+++ src/Board.js
@@ -18,6 +18,16 @@ class Board extends Component {
     this.nextId = this.nextId.bind(this);
   }
 
+  componentWillMount() {
+    let self = this;
+
+    if (this.props.count) {
+      fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+        .then(response => response.json())
+        .then(json => json[0].split('. ').forEach(sentence => self.add(sentence.substring(0, 25))))
+    }
+  }
+
   add(text) {
     this.setState(prevState => ({
       notes: [
